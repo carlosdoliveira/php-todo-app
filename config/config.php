0dd12e1d6d9c8596cc55d7b49db88c66982d6c85
Carlos Oliveira <cdaniel.oliveira@outlook.com>
2023-08-29 15:03:30 -0300

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+// Configurações de ambiente
+$dbHost = getenv('DB_HOST');
+$dbName = getenv('DB_NAME');
+$dbUser = getenv('DB_USER');
+$dbPass = getenv('DB_PASS');
+
+// Outras configurações
+// ...
+?>
