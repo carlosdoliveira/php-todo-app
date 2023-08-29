fab2a80b404f70ada14dd4bfeace80e06edbd2dc
Carlos Oliveira <cdaniel.oliveira@outlook.com>
2023-08-29 15:04:54 -0300

    Update Task.php

--- src/Model/Task.php
+++ src/Model/Task.php
@@ -7,6 +7,34 @@ class Task
     private $title;
     private $completed;
 
-    // Getters e setters aqui
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function isCompleted()
+    {
+        return $this->completed;
+    }
+
+    public function setCompleted($completed)
+    {
+        $this->completed = $completed;
+    }
 }
 ?>
