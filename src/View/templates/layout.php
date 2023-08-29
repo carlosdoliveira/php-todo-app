c952cc31e2f066a479d213e2f9920b22100480b1
Carlos Oliveira <cdaniel.oliveira@outlook.com>
2023-08-29 15:08:00 -0300

    Create layout.php

---
+++ src/View/templates/layout.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>To-do List</title>
+    <link rel="stylesheet" type="text/css" href="../../public/style.css">
+</head>
+<body>
+    <h1>To-do List</h1>
+    <div class="content">
+        <?php include $templatePath; ?>
+    </div>
+</body>
+</html>
