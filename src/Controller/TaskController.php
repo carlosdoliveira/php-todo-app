63bf7376e6165c845e326a82574b9c21faf613c1
Carlos Oliveira <cdaniel.oliveira@outlook.com>
2023-08-29 15:11:55 -0300

    Update TaskController.php

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -17,5 +17,43 @@ class TaskController
         $tasks = $this->taskRepository->getAllTasks();
         include '../src/View/templates/task_list.php';
     }
+
+    public function create()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $title = $_POST['title'];
+            $this->taskRepository->createTask($title);
+            header("Location: /");
+            exit;
+        }
+
+        include '../src/View/templates/create_task_form.php';
+    }
+
+    public function update($id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $title = $_POST['title'];
+            $completed = isset($_POST['completed']) ? 1 : 0;
+            $this->taskRepository->updateTask($id, $title, $completed);
+            header("Location: /");
+            exit;
+        }
+
+        $task = $this->taskRepository->getTaskById($id);
+        include '../src/View/templates/update_task_form.php';
+    }
+
+    public function delete($id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->taskRepository->deleteTask($id);
+            header("Location: /");
+            exit;
+        }
+
+        $task = $this->taskRepository->getTaskById($id);
+        include '../src/View/templates/delete_task_confirmation.php';
+    }
 }
 ?>
